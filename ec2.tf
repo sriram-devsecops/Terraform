feee4a7839f336f5915d6271db996c300f702ba1
sriram-devsecops <srirambhs46@gmail.com>
2025-07-26 03:25:39 +0530

    practice

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "allow-all" {
+  name        = "new-sg"
+  description = "desc-new-sg"
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow-all"
+  }
+}
