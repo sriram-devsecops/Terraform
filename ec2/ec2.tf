f7c0cd7c72afcb921ec76a209f3a4f8b9a75fdab
sriram-devsecops <srirambhs46@gmail.com>
2025-08-03 22:12:59 +0530

    practice

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "roboshop" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow-all.id]
   tags = var.ec2_tags
 }
--- ec2/sg copy.tf
+++ ec2/sg copy.tf
@@ -2,19 +2,21 @@ resource "aws_security_group" "allow-all" {
   name        = "allow-all"
   description = "Allow TLS inbound traffic and all outbound traffic"
   ingress {
-    from_port        = var.from_port
-    to_port          = var.to_port
+    from_port        = 0
+    to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = var.cidr_blocks
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
-    from_port        = var.from_port
-    to_port          = var.to_port
+    from_port        = 0
+    to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = var.cidr_blocks
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-  tags = var.sg_tags
+  tags = {
+    Name = "allow-all"
+  }
 }
